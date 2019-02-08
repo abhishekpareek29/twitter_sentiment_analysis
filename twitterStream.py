c1f6d8c99efb1d88dc2a78af1c573fd9de9a85fb
abhishekpareek29 <abhishekpareek29@gmail.com>
2019-02-08 07:53:19 -0500

    updated code

--- twitterStream.py
+++ twitterStream.py
@@ -1,23 +1,25 @@
-import findspark
-findspark.init()
-
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import operator
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
+
 
 def main():
     conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
     sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 10)   # Create a streaming context with batch interval of 10 sec
     ssc.checkpoint("checkpoint")
 
     pwords = load_wordlist("positive.txt")
     nwords = load_wordlist("negative.txt")
-   
+
     counts = stream(ssc, pwords, nwords, 100)
+
     make_plot(counts)
 
 
@@ -27,51 +29,70 @@ def make_plot(counts):
     Use plt.show() so that the plot will popup.
     """
     # YOUR CODE HERE
+    p_vals = []
+    n_vals = []
+
+    for x in counts:
+        if x :
+            p_vals.append(x[0][1])
+            n_vals.append(x[1][1])
+
+    plt.plot(p_vals,label='positive',marker='o')
+    plt.plot(n_vals,label='negative',marker='o')
+    plt.legend(loc='upper left')
+    plt.xticks(np.arange(0, len(counts), 1))
+    plt.ylim(ymin=0,ymax=max(max(p_vals),max(n_vals))+20)
+    plt.xlabel("Time Step")
+    plt.ylabel("Word Count")
+    plt.savefig("Plot")
 
 
 
 def load_wordlist(filename):
-    """ 
+    """
     This function should return a list or set of words from the given filename.
     """
-    f = open(filename, "r")
-    words = f.read().split('\n')
-    f.close()
-    return words
+    # YOUR CODE HERE
+    data=open(filename,'r')
+    data=data.read()
+    data=data.split('\n')
+    data = set(data)
 
+    return data
+
+def updateFunction(newValues, runningCount):
+    if runningCount is None:
+       runningCount = 0
+    return sum(newValues, runningCount)
 
 def stream(ssc, pwords, nwords, duration):
     kstream = KafkaUtils.createDirectStream(
         ssc, topics = ['twitterstream'], kafkaParams = {"metadata.broker.list": 'localhost:9092'})
-    tweets = kstream.map(lambda x: x[1])
-    tweets.pprint()
+    tweets = kstream.map(lambda x: x[1].encode("ascii","ignore"))
+
     # Each element of tweets will be the text of a tweet.
     # You need to find the count of all the positive and negative words in these tweets.
     # Keep track of a running total counts and print this at every time step (use the pprint function).
-    words = tweets.flatMap(lambda x: x.split(' '))
-    #words.pprint()
-    words = words.filter(lambda x: (x in pwords) or (x in nwords))
-    pairs = words.flatMap(lambda x: ("positive", 1) if (x in pwords) else ("negative", 1))
-    #pairs.pprint()
-    wordCount = pairs.reduceByKey(lambda x, y: x + y)
-    #wordCount.pprint()
-    
-    def updateFunction(newValues, runningCount):
-        if runningCount is None:
-            runningCount = 0
-        return sum(newValues, runningCount)
-    #totalcount = pairs.updateStateByKey(lambda x, y: x+y)
-    totalcount = pairs.updateStateByKey(updateFunction)
-    totalcount.pprint()
+    # YOUR CODE HERE
+    words_stream = tweets.map(lambda line: line.lower()).flatMap(lambda line: line.split(" "))
+    positive_words = words_stream.filter(lambda x: x in pwords)
+    negative_words = words_stream.filter(lambda x: x in nwords)
+
+    p_count = positive_words.map(lambda x: ('positive',1)).reduceByKey(lambda x, y: x + y)
+    n_count = negative_words.map(lambda x: ('negative',1)).reduceByKey(lambda x, y: x + y)
+    wordCount = p_count.union(n_count)
+
+    runningCounts = wordCount.updateStateByKey(updateFunction)
+
+    runningCounts.pprint()
+
     # Let the counts variable hold the word counts for all time steps
     # You will need to use the foreachRDD function.
     # For our implementation, counts looked like:
     #   [[("positive", 100), ("negative", 50)], [("positive", 80), ("negative", 60)], ...]
     counts = []
-    
-    #pairscount.foreachRDD(lambda t,rdd: counts.append(rdd.collect()))
-    
 
+    wordCount.foreachRDD(lambda t,rdd: counts.append(rdd.collect()))
     ssc.start()                         # Start the computation
     ssc.awaitTerminationOrTimeout(duration)
     ssc.stop(stopGraceFully=True)
@@ -80,4 +101,4 @@ def stream(ssc, pwords, nwords, duration):
 
 
 if __name__=="__main__":
-    main()										
+    main()
